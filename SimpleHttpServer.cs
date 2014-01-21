5c77403cde3b697608cbe6174dedc489112ca0a9
Paul Parker <paul@akirisolutions.com>
2014-01-20 19:41:13 -0800

    Minor change

--- SimpleHttpServer.cs
+++ SimpleHttpServer.cs
@@ -197,6 +197,23 @@ namespace Bend.Util {
             listener.Start();  //ERROR HERE: "The process cannot access the file because it is being used by another process"
             Console.WriteLine("Listening...");
 
+            // Note: The GetContext method blocks while waiting for a request. 
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response. 
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+
+
             //listener = new TcpListener(ip, port);
 
             while (is_active) {                
