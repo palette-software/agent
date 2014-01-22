881842d3e174aec937b8b4526a0199eb0bad67a9
Paul Parker <paul@akirisolutions.com>
2014-01-21 19:05:06 -0800

    Finally something that works

--- WindowsAgent.cs
+++ WindowsAgent.cs
@@ -7,19 +7,22 @@ using System.Configuration;
 using System.Diagnostics;
 
 using Bend.Util;
+using Palette;
 
 public class Agent
 {
     public static int Main(String[] args)
     {
+        //MyWebServer webserv = new MyWebServer();
+
         ConnectToController("localhost");
 
-        int port = 8081;
-        Bend.Util.MyHttpServer controller = new Bend.Util.MyHttpServer(port);
+        //int port = 8081;
+        //Bend.Util.MyHttpServer controller = new Bend.Util.MyHttpServer(port);
 
-        controller.connect();
+        //controller.connect();
 
-        HttpProcessor processor = new HttpProcessor(, controller); 
+        //HttpProcessor processor = new HttpProcessor(, controller); 
 
         return 0;
     }
@@ -32,15 +35,17 @@ public class Agent
             Encoding ASCII = Encoding.ASCII;
             string Post = "Agent version 0 running on " + server + "\n";
             Byte[] BytePost = ASCII.GetBytes(Post);
-            Byte[] RecvBytes = new Byte[256];
+            Byte[] RecvBytes = new Byte[1024];
             String strRetData = null;
+            int iStartPos = 0;
+            String sRequest;
 
             // Define those variables to be evaluated in the next for loop and 
             // then used to connect to the server. These variables are defined
             // outside the for loop to make them accessible there after.
             Socket sock = null;
             IPEndPoint hostEndPoint;
-            IPAddress hostAddress = null;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
             int conPort = 8081;
 
             // Get DNS host information.
@@ -49,69 +54,30 @@ public class Agent
             //IPaddress = ipHostInfo.AddressList[0];
 
             Console.WriteLine("Attempting to connect to host name : " + hostInfo.HostName);
-            Console.WriteLine("\n With IP address List : ");
-            for (int index = 0; index < hostInfo.AddressList.Length; index++)
-            {
-                Console.WriteLine(hostInfo.AddressList[index]);
-            }
-
-            // Evaluate the socket and receiving host IPAddress and IPEndPoint. 
-            //hostAddress = hostInfo.AddressList[1];
-            //hostEndPoint = new IPEndPoint(hostAddress, conPort);
-
-            // IPAddress and IPEndPoint represent the endpoint that will receive the request.
+            Console.WriteLine("\n With IP address : " + localAddr.ToString());
 
             // Creates the Socket to send data over a TCP connection.
             sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             string connectedIP = null;
 
-            foreach (IPAddress ipaddress in hostInfo.AddressList)
+            hostEndPoint = new IPEndPoint(localAddr, conPort);
+
+            sock.Connect(hostEndPoint);
+
+            if (!sock.Connected)
             {
-                // Connect to the host using its IPEndPoint.
-
-                //Ignore the IPV6 Loopback address
-                if (ipaddress.ToString() != "::1")
-                {
-                    hostAddress = ipaddress;
-                    hostEndPoint = new IPEndPoint(hostAddress, conPort);
-
-                    sock.Connect(hostEndPoint);
-
-                    if (!sock.Connected)
-                    {
-                        // Connection failed, try next IPaddress.
-                        Console.WriteLine("Unable to connect to host " + ipaddress.ToString());
-                        sock = null;                        
-                    }
-                    else
-                    {
-                        Console.WriteLine("Connected to host " + ipaddress.ToString());
-                        connectedIP = ipaddress.ToString();
-                        break;
-                    }
-                }
+                // Connection failed, try next IPaddress.
+                Console.WriteLine("Unable to connect to host " + localAddr.ToString());
+                sock = null;                        
+            }
+            else
+            {
+                Console.WriteLine("Connected to host " + localAddr.ToString());
+                connectedIP = localAddr.ToString();
+                //break;
             }
 
-            
-
-            /*
-
-            //Stream inputStream = new BufferedStream(socket.GetStream());
-
-            //// we probably shouldn't be using a streamwriter for all output from handlers either
-            //StreamWriter outputStream = new StreamWriter(new BufferedStream(socket.GetStream()));
-
-            //String request = streamReadLine(inputStream);
-            //string[] tokens = request.Split(' ');
-            //if (tokens.Length != 3)
-            //{
-            //    throw new Exception("invalid http request line");
-            //}
-            //http_method = tokens[0].ToUpper();
-            //http_url = tokens[1];
-            //http_protocol_versionstring = tokens[2];
-
-            //Console.WriteLine("starting: " + request);
+            //Console.WriteLine("starting: " + request);  
 
             // Send the data to the host.
             sock.Send(BytePost, BytePost.Length, 0);
@@ -120,6 +86,7 @@ public class Agent
             Int32 bytes = sock.Receive(RecvBytes, RecvBytes.Length, 0);
             strRetData = strRetData + ASCII.GetString(RecvBytes, 0, bytes);
 
+            //Use this only if we need to handle longer messages than 1024 bytes
             //while (bytes > 0)
             //{
             //    bytes = sock.Receive(RecvBytes, RecvBytes.Length, 0);
@@ -127,7 +94,37 @@ public class Agent
             //}
 
             Console.WriteLine("Received from " + connectedIP.ToString() + ": " + strRetData);
-  
+
+            //At present we will only deal with GET type
+            if (strRetData.Substring(0, 3) != "GET")
+            {
+                Console.WriteLine("Only Get Method is supported..");
+                sock.Close();
+                return;
+            }
+            // Look for HTTP request
+            iStartPos = strRetData.IndexOf("HTTP", 1);
+            // Get the HTTP text and version e.g. it will return "HTTP/1.1"
+            string sHttpVersion = strRetData.Substring(iStartPos, 8);
+            // Extract the Requested Type and Requested file/directory
+            sRequest = strRetData.Substring(0, iStartPos - 1);
+            //Replace backslash with Forward Slash, if Any
+            sRequest.Replace("\\", "/");
+            //If file name is not supplied add forward slash to indicate 
+            //that it is a directory and then we will look for the 
+            //default file name..
+            if ((sRequest.IndexOf(".") < 1) && (!sRequest.EndsWith("/")))
+            {
+                sRequest = sRequest + "/";
+            }
+            //Extract the requested file name
+            iStartPos = sRequest.LastIndexOf("/") + 1;
+            //sRequestedFile = sRequest.Substring(iStartPos);
+            //Extract The directory Name
+            //sDirName = sRequest.Substring(sRequest.IndexOf("/"), sRequest.LastIndexOf("/") - 3);
+            
+               
+            /*  
 
             //Put these in a config file or database for version 0
             string tableauFolder = @"C:\Program Files\Tableau\";
@@ -139,8 +136,6 @@ public class Agent
             string arguments = jobName + " " + dataFolder;  //@"backup C:\ProgramData\Tableau\Tableau Server\data\tableau_2014011401";
 
             SpawnProcess(binFolder, processName, jobName, arguments, dataFolder, outputFolder);
-
-          
              
             */
 
