56df2ea89e56684a81e6cd9e989875f6d60440b7
Paul Parker <paul@akirisolutions.com>
2014-01-25 10:17:01 -0800

    process classes

--- AgentProcess.cs
+++ AgentProcess.cs
@@ -7,32 +7,106 @@ using System.Net.Sockets;
 using System.Configuration;
 using System.Diagnostics;
 
-namespace Palette
-{
-    class AgentProcess
+public class CLIProcess : AgentProcess
+{   
+    public CLIProcess(string binaryFolder, string outputFolder, string command, string args)
     {
-    //    public static void SpawnProcess(string binFolder, string processName, string jobName, string arguments, string dataFolder, string outputFolder)
-    //    {
-    //        Process tableauOperation = new Process();
-    //        tableauOperation.StartInfo.FileName = binFolder + processName;
-    //        tableauOperation.StartInfo.Arguments = arguments + "tableau_" + jobName + "_" + System.DateTime.Now.ToString("yyyy.mm.dd");
-    //        tableauOperation.StartInfo.UseShellExecute = false;
-    //        tableauOperation.StartInfo.RedirectStandardOutput = true;
-    //        tableauOperation.Start();
+        this.binFolder = binFolder;
+        this.outputFolder = outputFolder;
+        this.command = command;  //i.e. "tabadmin backup"  
+        this.commandArgs = args;
 
-    //        string outputFileName = outputFolder + "tableau_" + jobName + "_" + System.DateTime.Now.ToString("yyyy.mm.dd") + ".out";
-
-    //        Console.WriteLine(tableauOperation.StandardOutput.ReadToEnd());
-
-    //        using (FileStream fs = new FileStream(outputFileName, FileMode.CreateNew))
-    //        {
-    //            using (BinaryWriter w = new BinaryWriter(fs))
-    //            {
-    //                w.Write(tableauOperation.StandardOutput.ReadToEnd());
-    //            }
-    //        }
-
-    //        tableauOperation.WaitForExit();
-    //    }
+        if (this.command == "tabadmin backup")
+        {
+            this.outputFileName = "tableau_backup " + System.DateTime.Now.ToString("yyyy.mm.dd") + ".out";
+        }
+        else if (this.command == "tabadmin restore")
+        {
+            this.outputFileName = "restore " + System.DateTime.Now.ToString("yyyy.mm.dd") + ".out";
+        }
     }
 }
+
+public abstract class AgentProcess
+{
+    protected int xid;  //controller transaction id
+    protected Process process;
+    protected string command;
+    protected string commandArgs = null;
+    protected int windows_process_id;  //actual windows process id
+    protected string processStatus;
+    protected DateTime processStartTime
+    {
+        get { return process.StartTime; }
+    }
+    protected TimeSpan totalProcessorTime
+    {
+        get { return process.TotalProcessorTime; }
+    }
+    protected DateTime lastCheckTime;
+    protected enum CommandType
+    {
+        status,
+        backup,
+        restore,
+        copy
+    }
+    protected string binFolder;
+    protected string outputFolder;
+    protected string outputFileName;
+
+    protected virtual int StartProcess()
+    {
+        process = new Process();
+        windows_process_id = process.Id;
+        process.StartInfo.FileName = binFolder + command;
+        process.StartInfo.Arguments = (commandArgs != null)? (" " + commandArgs + " " + outputFileName): outputFileName;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.Start();
+
+        Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+        using (FileStream fs = new FileStream(outputFileName, FileMode.CreateNew))
+        {
+            using (BinaryWriter w = new BinaryWriter(fs))
+            {
+                w.Write(process.StandardOutput.ReadToEnd());
+            }
+        }
+
+        process.WaitForExit();
+
+        return 0;
+    }
+
+    public virtual string ReturnStatus() //TODO: use try/catch, Make more comprehensive
+    {
+        if (process.Responding)
+            return "Running for " + totalProcessorTime.Minutes + " minutes";
+        else
+            if (process.HasExited)
+            {
+                return "Process has exited";
+            }
+            else
+            {
+                return "Not Responding";
+            }
+    }
+
+    public virtual void KillProcess()
+    {
+        try
+        {
+            process.Kill();
+        }
+        catch (Exception ex) 
+        {
+            //TODO: Need to handle this smoothly
+            Console.WriteLine(ex.ToString());
+        }
+        
+    }
+}
+
