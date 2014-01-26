b6093aa866118c70374a0e7be0d8f8fbd7235348
Paul Parker <paul@akirisolutions.com>
2014-01-25 16:34:31 -0800

    Made some changes as per spec

--- AgentProcess.cs
+++ AgentProcess.cs
@@ -8,12 +8,23 @@ using System.Configuration;
 using System.Diagnostics;
 
 public class CLIProcess : AgentProcess
-{   
+{
+    public CLIProcess(int xid) : base(xid) 
+    { 
+    }
+
+    /// <summary>
+    /// Process class for requests of type /cli
+    /// </summary>
+    /// <param name="binaryFolder">folder for command executable</param>
+    /// <param name="outputFolder">folder for output file</param>
+    /// <param name="command">the actual command (i.e. "tabadmin backup")</param>
+    /// <param name="args">command args (optional)</param>
     public CLIProcess(string binaryFolder, string outputFolder, string command, string args)
     {
-        this.binFolder = binFolder;
+        this.binFolder = binFolder;  
         this.outputFolder = outputFolder;
-        this.command = command;  //i.e. "tabadmin backup"  
+        this.command = command;  
         this.commandArgs = args;
 
         if (this.command == "tabadmin backup")
@@ -29,7 +40,9 @@ public class CLIProcess : AgentProcess
 
 public abstract class AgentProcess
 {
-    protected int xid;  //controller transaction id
+    const int SLEEP_AMOUNT = 100;
+    public int xid;  //controller transaction id
+    protected static Dictionary <int, int> xidMapping; 
     protected Process process;
     protected string command;
     protected string commandArgs = null;
@@ -44,16 +57,41 @@ public abstract class AgentProcess
         get { return process.TotalProcessorTime; }
     }
     protected DateTime lastCheckTime;
-    protected enum CommandType
-    {
-        status,
-        backup,
-        restore,
-        copy
-    }
+    int runStatus;
+    bool waitForResults;
     protected string binFolder;
     protected string outputFolder;
     protected string outputFileName;
+    bool eventHandled = false;
+
+    public AgentProcess()
+    {
+    }
+
+    public AgentProcess(int xid)
+    {
+        this.xid = xid;
+        if (xidMapping == null) xidMapping = new Dictionary<int, int>();
+    }
+
+    /// <summary>
+    /// Returns the Windows process id for a given controller process id
+    /// </summary>
+    /// <param name="xid">controller process id</param>
+    /// /// <param name="waitForResults">whether or not to wait for results</param>
+    /// <returns>Windows process id</returns>
+    protected int GetWindowsProcessForXid(int xid, bool waitForResults)
+    {
+        this.waitForResults = waitForResults;
+        if (xidMapping.ContainsKey(xid))
+        {
+            return xidMapping[xid];
+        }
+        else
+        {
+            return -1;
+        }
+    }
 
     protected virtual int StartProcess()
     {
@@ -63,50 +101,90 @@ public abstract class AgentProcess
         process.StartInfo.Arguments = (commandArgs != null)? (" " + commandArgs + " " + outputFileName): outputFileName;
         process.StartInfo.UseShellExecute = false;
         process.StartInfo.RedirectStandardOutput = true;
-        process.Start();
 
-        Console.WriteLine(process.StandardOutput.ReadToEnd());
-
-        using (FileStream fs = new FileStream(outputFileName, FileMode.CreateNew))
+        try
         {
-            using (BinaryWriter w = new BinaryWriter(fs))
+            process.Start();
+
+            if (!xidMapping.ContainsKey(xid)) xidMapping.Add(xid, windows_process_id);
+
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+            using (FileStream fs = new FileStream(outputFileName, FileMode.CreateNew))
             {
-                w.Write(process.StandardOutput.ReadToEnd());
-            }
+                using (BinaryWriter w = new BinaryWriter(fs))
+                {
+                    w.Write(process.StandardOutput.ReadToEnd());
+                }
+            }            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception caught in spawning process!! " + ex.ToString());
+            runStatus = -1;  //Not currently part of spec, but should be?
         }
 
-        process.WaitForExit();
+        if (waitForResults)
+        {
+            process.Exited += new EventHandler(myProcess_Exited);
+            while (!eventHandled)
+            {
+                process.WaitForExit(SLEEP_AMOUNT);
+            }
 
-        return 0;
+            runStatus = 1;
+        }
+        else
+        {
+            runStatus = 1;
+        }
+
+        return runStatus;        
     }
 
-    public virtual string ReturnStatus() //TODO: use try/catch, Make more comprehensive
+    // Handle Exited event and display process information. 
+    private void myProcess_Exited(object sender, System.EventArgs e)
+    {
+        eventHandled = true;
+        Console.WriteLine("Exit time: {0}\r\n" +
+            "Exit code: {1}\r\nElapsed time: {2}", process.ExitTime, process.ExitCode, totalProcessorTime.Seconds);
+    }
+
+    public virtual int ReturnStatus() 
     {
         if (process.Responding)
-            return "Running for " + totalProcessorTime.Minutes + " minutes";
+            return 1;
         else
-            if (process.HasExited)
+            if (eventHandled)
             {
-                return "Process has exited";
+                return 2;
+            }
+            else if (runStatus == 3)
+            {
+                return 3;
             }
             else
             {
-                return "Not Responding";
+                return -1;
             }
     }
 
-    public virtual void KillProcess()
+    public virtual int KillProcess()
     {
         try
         {
-            process.Kill();
+            if (process.Responding)
+            {
+                process.Kill();
+                runStatus = 3;
+            }
         }
         catch (Exception ex) 
         {
-            //TODO: Need to handle this smoothly
-            Console.WriteLine(ex.ToString());
+            runStatus = -1;
+            Console.WriteLine("Error in Killing process " + xid + " " + ex.ToString());
         }
-        
+        return runStatus;
     }
 }
 
