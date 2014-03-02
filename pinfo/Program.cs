e027c769f03d11a3018df44ebae076dce1edd053
Paul Parker <paul@akirisolutions.com>
2014-03-02 11:04:17 -0800

    Now writes nested JSON data to StdOut, for example,
    {"C:\":{"Name":"C:\\","Drive type":"Fixed","Volume label":"Gateway","Drive format":"NTFS","Available space to current user":360975884288,"Total available space":360975884288,"Total size of drive":1986333331456},"D:\":{"Name":"D:\\","Drive type":"CDRom"},"F:\":{"Name":"F:\\","Drive type":"Removable"},"G:\":{"Name":"G:\\","Drive type":"Removable"},"H:\":{"Name":"H:\\","Drive type":"Fixed","Volume label":"JUMBO DRIVE","Drive format":"NTFS","Available space to current user":299738214400,"Total available space":299738214400,"Total size of drive":2000396288000}}

--- pinfo/Program.cs
+++ pinfo/Program.cs
@@ -6,78 +6,68 @@ using System.IO;
 using fastJSON;
 using System.Reflection;
 
-    class Program
+class Program
+{
+    /// <summary>
+    /// Writes a serialized JSON string to StdOut with usage information for each drive on the system
+    /// </summary>
+    /// <param name="args">currently only /du to return disk usage information</param>
+    /// <returns>nested JSON</returns>
+    static int Main(string[] args)
     {
-        static int Main(string[] args)
+        if (args.Length < 1)
         {
-            if (args.Length < 1)
-            {
-                //Example: pinfo du (returns disk usage)
-                Console.WriteLine("Usage: pinfo.exe /du");
-                return -1;
-            }
-            else if (args[0] == @"/du")
-            {
-                Dictionary<string, object> data = GetDriveInfo();
+            //Example: pinfo /du (returns disk usage)
+            Console.Error.WriteLine("Usage: pinfo.exe /du");
+            return -1;
+        }
+        else if (args[0] == @"/du")
+        {
+            Dictionary<string, Dictionary<string, object>> data = GetDriveInfo();
 
-                string json = fastJSON.JSON.Instance.ToJSON(data);
+            string json = fastJSON.JSON.Instance.ToJSON(data);
 
-                Console.Out.WriteLine(json);
+            Console.Out.WriteLine(json);
 
-                return 0;
-            }
-
-            else
-            {
-                Console.WriteLine("Usage: pinfo.exe /du");
-                return -1;
-            }
+            return 0;
         }
 
-        private static Dictionary<string, object> GetDriveInfo()
+        else
         {
-            Dictionary<string, object> data = new Dictionary<string, object>();
-
-            //Get the current drive
-            string driveLetter = Path.GetPathRoot(Environment.CurrentDirectory);
-
-            //return dictionary for primary drive only
-            DriveInfo di = new DriveInfo(driveLetter);
-
-            data["Name"] = di.Name;
-            data["Drive type"] = di.DriveType;
-            data["Volume label"] = di.VolumeLabel;
-            data["Drive format"] = di.DriveFormat;
-            data["Available space to current user"] = di.AvailableFreeSpace;
-            data["Total available space"] = di.TotalFreeSpace;
-            data["Total size of drive"] = di.TotalSize;
-
-            return data;
-
-            /* IF WE NEED TO GET SPACE FOR ALL DRIVES, USE THIS 
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
-
-            foreach (DriveInfo d in allDrives)
-            {
-                Console.WriteLine("Drive {0}", d.Name);
-                Console.WriteLine("  File type: {0}", d.DriveType);
-                if (d.IsReady == true)
-                {
-                    Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
-                    Console.WriteLine("  File system: {0}", d.DriveFormat);
-                    Console.WriteLine(
-                        "  Available space to current user:{0, 15} bytes",
-                        d.AvailableFreeSpace);
-
-                    Console.WriteLine(
-                        "  Total available space:          {0, 15} bytes",
-                        d.TotalFreeSpace);
-
-                    Console.WriteLine(
-                        "  Total size of drive:            {0, 15} bytes ",
-                        d.TotalSize);
-                } 
-            }
-            */
+            Console.Error.WriteLine("Usage: pinfo.exe /du");
+            return -1;
         }
     }
+
+    /// <summary>
+    /// Returns a nested dictionary of drive info for each drive
+    /// </summary>
+    /// <returns>a nested dictionary</returns>
+    private static Dictionary<string, Dictionary<string, object>> GetDriveInfo()
+    {
+        //Use a nested dictionary to handle mutliple drives
+        Dictionary<string, Dictionary<string, object>> allData = new Dictionary<string, Dictionary<string, object>>();
+
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        foreach (DriveInfo di in allDrives)
+        {
+            Dictionary<string, object> driveData = new Dictionary<string, object>();
+            driveData["Name"] = di.Name;
+            driveData["Drive type"] = di.DriveType;
+
+            if (di.IsReady == true)
+            {
+                driveData["Volume label"] = di.VolumeLabel;
+                driveData["Drive format"] = di.DriveFormat;
+                driveData["Available space to current user"] = di.AvailableFreeSpace;
+                driveData["Total available space"] = di.TotalFreeSpace;
+                driveData["Total size of drive"] = di.TotalSize;
+            }
+
+            allData.Add(di.Name, driveData);
+        }
+
+        return allData;
+    }
+}
