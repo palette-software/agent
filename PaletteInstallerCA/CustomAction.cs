2f1cfbd23c0edc0307490da6617bfe849029d1a2
Patrick J. McEvoy <swagman@swagman.com>
2014-04-11 15:37:07 -0700

    change log to logs

--- PaletteInstallerCA/CustomAction.cs
+++ PaletteInstallerCA/CustomAction.cs
@@ -116,7 +116,7 @@ namespace PaletteInstallerCA
                 output += "# port=8889" + Environment.NewLine;
                 output += Environment.NewLine;
                 output += "[logging]" + Environment.NewLine;
-                output += "location=" + installDir + @"log\agent.log" + Environment.NewLine;
+                output += "location=" + installDir + @"logs\agent.log" + Environment.NewLine;
                 output += "maxsize=10MB" + Environment.NewLine;
 
                 string inipath = Path.Combine(installDir, @"conf\agent.ini");
