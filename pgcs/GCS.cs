d9ba07a3a1be7d172a15d4eb2f16edd3dddf8c3b
Matthew R. Laue <matt@akirisolutions.com>
2014-06-21 17:12:58 -0700

    First implementation of the pgcs executable for interacting with Google Cloud Storage.

---
+++ pgcs/GCS.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Security.Cryptography;
+
+class GCS
+{
+    public const string DEFAULT_TARGET = "commondatastorage.googleapis.com";
+
+    private string accessKey;
+    private string secretKey;
+    private string target;
+
+    public GCS(string accessKey, string secretKey, string target)
+    {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        if (target == null || target == String.Empty)
+        {
+            this.target = DEFAULT_TARGET;
+        }
+    }
+
+    public GCS(string accessKey, string secretKey) : this(accessKey, secretKey, null) { }
+
+    private void setDate(HttpWebRequest req, DateTime dateTime)
+    {
+        //string date = String.Format("{0:ddd,' 'dd' 'MMM' 'yyyy' 'HH':'mm':'ss' 'K}", dateTime);
+        string date = dateTime.ToUniversalTime().ToString("r");
+        MethodInfo priMethod = req.Headers.GetType().GetMethod("AddWithoutValidate", BindingFlags.Instance | BindingFlags.NonPublic);
+        priMethod.Invoke(req.Headers, new[] {"Date", date});
+    }
+
+    public HttpWebRequest createWebRequest(string requestType, string bucketName, string objectName)
+    {
+        string requestUri = "http://" + bucketName + "." + target + "/" + objectName;
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(requestUri);
+        req.Method = requestType;
+        return req;
+    }
+
+    public int GET(string bucketName, string objectName, string path)
+    {
+        if (path == null)
+        {
+            path = objectName;
+        }
+
+        HttpWebRequest req = createWebRequest("GET", bucketName, objectName);
+        GCSAuthorization auth = new GCSAuthorization(this, req, bucketName, objectName);
+        setDate(req, auth.dateTime);
+        req.Headers.Add("Authorization: " + auth.ToString());
+
+        HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+        long contentLength = res.ContentLength;
+
+        BinaryReader reader = new BinaryReader(res.GetResponseStream());
+        byte[] data = reader.ReadBytes((int)contentLength);
+        reader.Close();
+
+        File.WriteAllBytes(path, data);
+        return (int)res.StatusCode;
+    }
+
+    public int PUT(string bucketName, string filePath)
+    {
+        string objectName = Path.GetFileName(filePath);
+
+        FileInfo fi = new FileInfo(filePath);
+        long contentLength = fi.Length;
+
+
+        HttpWebRequest req = createWebRequest("PUT", bucketName, objectName);
+        req.ContentType = "application/octet-stream";
+        req.ContentLength = fi.Length;
+
+        GCSAuthorization auth = new GCSAuthorization(this, req, bucketName, objectName);
+        setDate(req, auth.dateTime);
+        req.Headers.Add("Authorization: " + auth.ToString());
+
+        BinaryWriter writer = new BinaryWriter(req.GetRequestStream());
+        writer.Write(File.ReadAllBytes(filePath));
+        writer.Close();
+
+        HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+        return (int)res.StatusCode;
+    }
+
+    private class GCSAuthorization
+    {
+        private GCS gcs;
+        private HttpWebRequest req;
+        private string bucketName;
+        private string objectName;
+        public DateTime dateTime;
+
+        public GCSAuthorization(GCS gcs, HttpWebRequest req, string bucketName, string objectName, DateTime dateTime)
+        {
+            this.gcs = gcs;
+            this.req = req;
+            this.bucketName = bucketName;
+            this.objectName = objectName;
+            this.dateTime = dateTime;
+        }
+
+        public GCSAuthorization(GCS gcs, HttpWebRequest req, string bucketName, string objectName):
+            this(gcs, req, bucketName, objectName, DateTime.Now) { }
+
+        // https://developers.google.com/storage/docs/migrating#authentication
+        // "Expand to see the details of authentication"
+        public string CreateMessageToBeSigned()
+        {
+            String newline = "\n";
+            StringBuilder finalStr = new StringBuilder();
+            finalStr.Append(req.Method);
+            finalStr.Append(newline);
+            // Content-MD5
+            finalStr.Append(newline);
+            finalStr.Append(req.ContentType);
+            finalStr.Append(newline);
+            finalStr.Append(dateTime.ToUniversalTime().ToString("r"));
+            finalStr.Append(newline);
+            finalStr.Append("/");
+            finalStr.Append(bucketName);
+            finalStr.Append("/");
+            finalStr.Append(objectName);
+
+            return finalStr.ToString();
+        }
+
+        public String CreateSignature()
+        {
+            String messageToBeSigned = CreateMessageToBeSigned();
+
+            byte[] secretBytes = Encoding.UTF8.GetBytes(gcs.secretKey);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageToBeSigned);
+
+            HMACSHA1 myhmacsha1 = new HMACSHA1(secretBytes);
+            byte[] hashValue = myhmacsha1.ComputeHash(messageBytes);
+
+            return Convert.ToBase64String(hashValue);
+        }
+
+        public override string ToString()
+        {
+            return "GOOG1 " + gcs.accessKey + ":" + CreateSignature();
+        }
+    }
+}
---
+++ pgcs/pgcs.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+class pgcs
+{
+    static int usage()
+    {
+        Console.Error.WriteLine("usage: pgcs [GET|PUT] <bucket> <file-or-key>");
+        return -1;
+    }
+
+    static int doGET(GCS gcs, string bucketName, string key)
+    {
+        string path = key;
+        gcs.GET(bucketName, key, path);
+        return 0;
+    }
+
+    static int doPUT(GCS gcs, string bucketName, string path)
+    {
+        if (!File.Exists(path))
+        {
+            // This exception is also thrown if path specifies a non-regular file (e.g. a directory)
+            throw new FileNotFoundException(path);
+        }
+
+        int result = gcs.PUT(bucketName, path);
+        return result == 200 ? 0 : -1;
+    }
+
+    static int Main(string[] args)
+    {
+        //System.Diagnostics.Debugger.Launch();
+
+        if (args.Length != 3)
+        {
+            return usage();
+        }
+
+        string accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY");
+        if (accessKey == null)
+        {
+            Console.Error.WriteLine("[ERROR] The environment variable ACCESS_KEY is required.");
+            return -1;
+        }
+
+        string secretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
+        if (secretKey == null)
+        {
+            Console.Error.WriteLine("[ERROR] The environment variable SECRET_KEY is required.");
+            return -1;
+        }
+
+        GCS gcs = new GCS(accessKey, secretKey);
+
+        string method = args[0].ToUpper();
+        try
+        {
+            switch (method)
+            {
+                case "GET":
+                    return doGET(gcs, args[1], args[2]);
+                case "PUT":
+                    return doPUT(gcs, args[1], args[2]);
+                default:
+                    return usage();
+            }
+        }
+        catch (WebException e)
+        {
+            using (StreamReader reader = new StreamReader(e.Response.GetResponseStream()))
+            {
+                Console.Error.WriteLine(reader.ReadToEnd());
+            }
+            throw(e);
+        }
+    }
+}
