07a21846f560eed8f910c87a6cae42f6d98a8067
Paul Parker <paul@akirisolutions.com>
2014-10-07 21:27:16 -0700

    Changes for PD-3852: Add windows license validity to pinfo

--- pinfo/pinfo.cs
+++ pinfo/pinfo.cs
@@ -10,6 +10,8 @@ using System.Reflection;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
+using SLID = System.Guid;
+
 class pinfo
 {
     /// <summary>
@@ -17,6 +19,7 @@ class pinfo
     /// </summary>
     /// <param name="args">currently only /du to return disk usage information</param>
     /// <returns>nested JSON</returns>
+    /// 
     static int Main(string[] args)
     {
         Dictionary<string, object> allData = new Dictionary<string,object>();
@@ -47,6 +50,24 @@ class pinfo
                 }
             }
 
+            //For PD-3852
+            if (Environment.OSVersion.Version.Major >= 6) //Version 6 includes Vista, Server 2008, Windows 7, Windows 8, and Server 2012 
+            {
+                if (IsGenuineWindows())
+                {
+                    allData.Add("windows-license", "genuine windows");
+                }
+                else
+                {
+                    allData.Add("windows-license", "not genuine windows");
+                }
+            }
+            else
+            {
+                allData.Add("windows-license", "os not supported");
+            }
+            //End PD-3852
+
             string json = fastJSON.JSON.Instance.ToJSON(allData);
             Console.WriteLine(json);
         }
@@ -56,6 +77,45 @@ class pinfo
         }
 
         return 0;
+    }
+
+    public enum SL_GENUINE_STATE
+    {
+        SL_GEN_STATE_IS_GENUINE = 0,
+        SL_GEN_STATE_INVALID_LICENSE = 1,
+        SL_GEN_STATE_TAMPERED = 2,
+        SL_GEN_STATE_LAST = 3
+    }
+
+    [DllImportAttribute("Slwga.dll", EntryPoint = "SLIsGenuineLocal", CharSet = CharSet.None, ExactSpelling = false, SetLastError = false, PreserveSig = true, CallingConvention = CallingConvention.Winapi, BestFitMapping = false, ThrowOnUnmappableChar = false)]
+    [PreserveSigAttribute()]
+    internal static extern uint SLIsGenuineLocal(ref SLID slid, [In, Out] ref SL_GENUINE_STATE genuineState, IntPtr val3);
+
+
+    public static bool IsGenuineWindows()
+    {
+        bool _IsGenuineWindows = false;
+        Guid ApplicationID = new Guid("55c92734-d682-4d71-983e-d6ec3f16059f"); //Application ID GUID http://technet.microsoft.com/en-us/library/dd772270.aspx
+        SLID windowsSlid = (Guid)ApplicationID;
+        try
+        {
+            SL_GENUINE_STATE genuineState = SL_GENUINE_STATE.SL_GEN_STATE_LAST;
+            uint ResultInt = SLIsGenuineLocal(ref windowsSlid, ref genuineState, IntPtr.Zero);
+            if (ResultInt == 0)
+            {
+                _IsGenuineWindows = (genuineState == SL_GENUINE_STATE.SL_GEN_STATE_IS_GENUINE);
+            }
+            else
+            {
+                Console.WriteLine("Error getting information {0}", ResultInt.ToString());
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return _IsGenuineWindows;
     }
 
     /// <summary>
