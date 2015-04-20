e3ed7b2fbaeaf49700608a4cc574b18815f52d07
Matthew R. Laue <matt@palette-software.com>
2015-04-20 14:18:29 -0700

    Add a basic installer DLL to the MSI package with a first custom action.

---
+++ InstallerDLL/InstallerDLL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InstallShield.Interop;
+
+namespace InstallerDLL
+{
+    public class InstallerDLL
+    {
+        public static bool CheckLicenseKey(string key)
+        {
+            if (key.Length != 36)
+            {
+                return false;
+            }
+            if ((key[8] != '-') || (key[13] != '-') || (key[18] != '-') || (key[23] != '-'))
+            {
+                return false;
+            }
+            return true; 
+        }
+
+        public static int ValidateLicenseKey(string key)
+        {
+            //System.Diagnostics.Debugger.Launch();
+            if (!CheckLicenseKey(key))
+            {
+                MessageBox.Show("Invalid license key format\nThe license key should have the format: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'", "License Key Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                return 0;
+            }
+            // FIXME: check licensing
+            return 1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        public static void PrintValue(Int32 num)
+        {
+            string value = Convert.ToString(num);
+            MessageBox.Show(value, "Print Value", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// EchoCustomActionData retrieves the CustomActionData property and shows it in a messagebox; then aborts the setup
+        /// </summary>
+        public static void EchoCustomActionData(Int32 handle)
+        {
+            string data = Msi.CustomActionHandle(handle).GetProperty("CustomActionData");
+            MessageBox.Show(data, "EchoCustomActionData", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            throw new Exception("Aborting setup by throwing an exception");
+        }
+    }
+}
